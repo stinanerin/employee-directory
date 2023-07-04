1652963ddab1db1cbdd401b39a4d8aff1d2606d1
stormstina <stina.nerin@gmail.com>
2023-07-04 13:31:23 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <h1>Employee directory</h1>
+
+  <EmployeeList/>
+  
+</template>
+
+<script>
+
+import EmployeeList from "./components/EmployeeList.vue"
+
+
+export default {
+
+  name: 'App',
+
+  components: { EmployeeList }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,74 @@
+<template>
+    <ul class="employee-list">
+
+        <li v-for="employee in employees" :key="employee.id">
+            <img :src="employee.avatar" :alt="'Picture of ' + employee.first_name" />
+            <h2>
+                {{ employee.first_name }} {{ employee.last_name }}
+            </h2>
+            <a :href="'mailto:' + employee.email + '?&body=Hi%20' + employee.first_name + ','">Contact me!</a>
+
+        </li>
+
+    </ul>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+    data() {
+        return {
+        employees: [], // to store the fetched employee data
+        currentPage: 1, // current page of the pagination
+        };
+    },
+    mounted() {
+        this.fetchEmployees();
+    },
+    methods: {
+        async fetchEmployees() {
+        try {
+            const res = await axios.get(`https://reqres.in/api/users?page=${this.currentPage}`);
+            if(res.status !== 200) {
+                throw new Error()
+            }
+            const data = res.data
+            console.log(data);
+            this.employees = data.data;
+        } catch (error) {
+            console.error("Error fetchEmployees", error);
+        }
+        },
+    },
+    };
+
+    
+</script>
+
+<style>
+
+  .employee-list {
+    display:flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 3rem;
+    
+  }
+  .employee-list li {
+    list-style-type: none;
+    padding: 2rem;
+    background-color: var(--background-clr);
+    border-radius: 1rem;
+    text-align: center;
+    width: 200px;
+  }
+  .employee-list li img{
+    border-radius: 50%;
+
+  }
+  .employee-list li a {
+    color: var(--text-clr)
+  }
+  
+</style>
